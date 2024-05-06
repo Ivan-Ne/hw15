90db4bccce7a7f39080bd8e57ed264d92b197f17
neverov.iv@gmail.com <neverov.iv@gmail.com>
2024-05-06 23:25:16 +0300

    The 2nd commit today

--- tests/test_indirect.py
+++ tests/test_indirect.py
@@ -5,9 +5,11 @@ from models.github_page import github_page
 def test_github_desktop(setup_browser):
     github_page.open()
     github_page.desktop_sign_in()
+    github_page.should_have_text_sign_in()
 
 
 @pytest.mark.parametrize('setup_browser', ['mobile'], indirect=True)
 def test_github_mobile(setup_browser):
     github_page.open()
     github_page.mobile_sign_in()
+    github_page.should_have_text_sign_in()
